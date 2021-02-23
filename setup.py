1096925863881719f077ebf36e7ddf334be6b9bb
Rémi BRAUN <remi.braun@unistra.fr>
2021-02-23 15:26:18 +0100

    Creating proper package for eodag_sentinelsat

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 BASEDIR = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
 with open(os.path.join(BASEDIR, 'README.rst'), 'r') as f:
@@ -9,30 +8,28 @@ with open(os.path.join(BASEDIR, 'README.rst'), 'r') as f:
 
 setup(
     name="eodag_sentinelsat",
-    version="0.1.0",
+    version="0.2.0",
     description="Sentinelsat plugin to EODAG (https://bitbucket.org/geostorm/eodag)",
     long_description=readme,
     author="CS Systemes d'Information (CSSI)",
     author_email="admin@geostorm.eu",
     url="https://bitbucket.org/geostorm/eodag-sentinelsat",
     license="GPLv3",
-    entry_points={
-        'eodag.plugins.api': [
-            'SentinelsatAPI = eodag_sentinelsat:SentinelsatAPI',
-        ]
-    },
-    py_modules=['eodag_sentinelsat'],
+    packages=find_packages(),
     install_requires=[
         'tqdm',
         'sentinelsat',
-        'shapely',
+        'eodag',
+        'shapely'
     ],
     extras_require={
-        'standalone': [
-            'eodag',
-        ],
         'dev': [
             'tox',
+        ]
+    },
+    entry_points={
+        'eodag.plugins.api': [
+            'SentinelsatAPI = eodag_sentinelsat:SentinelsatAPI',
         ]
     },
     project_urls={
@@ -40,16 +37,17 @@ setup(
         "Documentation": "https://bitbucket.org/geostorm/eodag-sentinelsat/src/master/README.rst",
         "Source Code": "https://bitbucket.org/geostorm/eodag-sentinelsat",
     },
-    classifiers=(
+    classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Scientific/Engineering :: GIS',
-    ),
+    ],
+    package_data={'eodag_sentinelsat': ['*.yml']},
+    include_package_data=True
 )
