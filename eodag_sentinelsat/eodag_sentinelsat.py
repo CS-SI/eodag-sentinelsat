5dbd802d54be361cee0782e71bfe4180581b4570
Rémi BRAUN <remi.braun@unistra.fr>
2021-03-18 16:52:21 +0100

    Updates according to version 0.2.0

--- eodag_sentinelsat/eodag_sentinelsat.py
+++ eodag_sentinelsat/eodag_sentinelsat.py
@@ -14,22 +14,26 @@
 #
 #     You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-from __future__ import unicode_literals
+"""Sentinelsat plugin to EODAG."""
+
+import ast
 import logging
 import zipfile
 from datetime import datetime
 
+from eodag.plugins.apis.base import Api
 from eodag.plugins.search.qssearch import ODataV4Search
+from eodag.utils import get_progress_callback
 from eodag.utils.exceptions import RequestError, MisconfiguredError
 from sentinelsat import SentinelAPI, SentinelAPIError
-from tqdm import tqdm
-from eodag.plugins.apis.base import Api
 
 logger = logging.getLogger('eodag.plugins.apis.sentinelsat')
 
 
 class SentinelsatAPI(Api, ODataV4Search):
     """
+    SentinelsatAPI plugin.
+
     Api that enables to search and download EO products from catalogs implementing the SchiHub interface.
     It is basically a wrapper around sentinelsat, enabling it to be used on eodag.
 
@@ -45,21 +49,7 @@ class SentinelsatAPI(Api, ODataV4Search):
         super().__init__(provider, config)
         self.api = None
 
-    @staticmethod
-    def from_prodtype_to_platform(prod_type):
-        if prod_type.startswith("S1"):
-            return "Sentinel-1"
-
-        elif prod_type.startswith("S2"):
-            return "Sentinel-2"
-
-        elif prod_type.startswith("S3"):
-            return "Sentinel-3"
-
-        else:
-            raise Exception("Invalid producttype for Sentinelsat AOI")
-
-    def query(self, product_type=None, *args, **kwargs):
+    def query(self, items_per_page=None, page=None, count=True, **kwargs):
         """
         Query
         :param product_type: (str) Product Type, not used, just here for compatibility reasons
@@ -67,14 +57,25 @@ class SentinelsatAPI(Api, ODataV4Search):
         :return: (list, int) List and number of queried products
         """
         eo_products = []
-        product_type = kwargs.get("productType")
+        product_type = kwargs.get("productType", None)
         if product_type is not None:
             # Init Sentinelsat API (connect...)
             self._init_api()
 
             # Modify the query parameters to be compatible with Sentinelsat query
-            kwargs["platformname"] = self.from_prodtype_to_platform(product_type)
-            query_params, provider_product_type = self.update_keyword(*args, **kwargs)
+            query_params, provider_product_type = self.update_keyword(**kwargs)
+
+            # add pagination
+            pagination_params_str = self.config.pagination.get(
+                "next_page_query_obj", {}
+            ).format(
+                items_per_page=items_per_page,
+                page=page,
+                skip=items_per_page * (page - 1),
+            )
+            pagination_params = ast.literal_eval(pagination_params_str)
+            query_params.update(pagination_params)
+
             try:
                 # Query
                 results = self.api.query(**query_params)
@@ -84,10 +85,7 @@ class SentinelsatAPI(Api, ODataV4Search):
                     res["storage_status"] = self.api.is_online(uuid)
 
                 # Normalize results skeletons (using providers.yml file)
-                eo_products = self.normalize_results(results.values(),
-                                                     product_type,
-                                                     provider_product_type,
-                                                     *args, **kwargs)
+                eo_products = self.normalize_results(results.values(), **kwargs)
 
             except TypeError:
                 import traceback as tb
@@ -130,7 +128,7 @@ class SentinelsatAPI(Api, ODataV4Search):
         # Mange the case if nothing has been downloaded
         return prods[0] if len(prods) > 0 else ""
 
-    def download_all(self, product, auth=None, progress_callback=None, **kwargs) -> list:
+    def download_all(self, search_result, auth=None, progress_callback=None, **kwargs) -> list:
         """
         Download products
 
@@ -145,35 +143,37 @@ class SentinelsatAPI(Api, ODataV4Search):
         self._init_api()
 
         # Download all products
-        prod_ids = [prod.properties["uuid"] for prod in product.data]
+        prod_ids = [prod.properties["uuid"] for prod in search_result.data]
         success, _, _ = self.api.download_all(prod_ids, directory_path=self.config.outputs_prefix)
 
         # Only extract the successfully downloaded products
         paths = [self.extract(prods) for prods in success.values()]
         return paths
 
-    def extract(self, prod_info: dict) -> str:
+    def extract(self, product_info: dict) -> str:
         """
         Extract products if needed
 
-        :param prod_info: Product info
+        :param product_info: Product info
         :return: Path (archive or extracted according to the config)
         """
         # Extract them if needed
-        if self.config.extract and prod_info['path'].endswith('.zip'):
-            logger.info('Extraction activated')
-            with zipfile.ZipFile(prod_info['path'], 'r') as zfile:
+        if self.config.extract and product_info["path"].endswith(".zip"):
+            logger.info("Extraction activated")
+            with zipfile.ZipFile(product_info["path"], "r") as zfile:
                 fileinfos = zfile.infolist()
-                with tqdm(fileinfos,
-                          unit='file',
-                          desc='Extracting files from {}'.format(prod_info['path'])) as progressbar:
-                    for fileinfo in progressbar:
+                with get_progress_callback() as bar:
+                    bar.max_size = len(fileinfos)
+                    bar.unit = "file"
+                    bar.desc = "Extracting files from {}".format(product_info["path"])
+                    bar.unit_scale = False
+                    bar.position = 2
+                    for fileinfo in fileinfos:
                         zfile.extract(fileinfo, path=self.config.outputs_prefix)
-            path = prod_info['path'][:prod_info['path'].index('.zip')]
+                        bar(1)
+            return product_info["path"][: product_info["path"].index(".zip")]
         else:
-            path = prod_info['path']
-
-        return path
+            return product_info["path"]
 
     def _init_api(self) -> None:
         """ Initialize Sentinelsat API if needed (connection and link) """
@@ -190,28 +190,31 @@ class SentinelsatAPI(Api, ODataV4Search):
         else:
             logger.debug('Sentinelsat API already initialized')
 
-    def update_keyword(self, *args, **kwargs):
+    def update_keyword(self, **kwargs):
         """
         Update keywords for SentinelSat API
         """
         product_type = kwargs.get("productType", None)
-        provider_product_type = self.map_product_type(product_type, *args, **kwargs)
+        provider_product_type = self.map_product_type(product_type, **kwargs)
         keywords = {k: v for k, v in kwargs.items() if k != "auth" and v is not None}
         keywords["productType"] = provider_product_type
 
         # Add to the query, the queryable parameters set in the provider product type definition
         product_type_def_params = self.get_product_type_def_params(
-            product_type, *args, **kwargs
+            product_type, **kwargs
         )
         keywords.update(
             {
                 k: v
-                for k, v in product_type_def_params.items() if (k not in keywords.keys()
-                                                                and k in self.config.metadata_mapping.keys()
-                                                                and isinstance(self.config.metadata_mapping[k], list))
+                for k, v in product_type_def_params.items()
+                if (
+                k not in keywords.keys()
+                and k in self.config.metadata_mapping.keys()
+                and isinstance(self.config.metadata_mapping[k], list)
+            )
             }
         )
-        qp, qs = self.build_query_string(product_type, *args, **keywords)
+        qp, qs = self.build_query_string(product_type, **keywords)
 
         # If we were not able to build query params but have search criteria, this means
         # the provider does not support the search criteria given. If so, stop searching
